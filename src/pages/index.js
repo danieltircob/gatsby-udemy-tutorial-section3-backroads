9beed77963ba3ee497e0922630f9e19193970d5f
Tircob Daniel <d.tircob95@gmail.com>
2019-07-23 11:09:59 +0200

    everything until adding styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,19 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import SimleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+export default () => (
+  <Layout>
+    <SimleHero>
+      <Banner
+        title="continue exploring"
+        info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
+      >
+        <Link to="/tours" className="btn-white">
+          explore tours
+        </Link>
+      </Banner>
+    </SimleHero>
+  </Layout>
+)
